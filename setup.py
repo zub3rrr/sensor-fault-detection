694c02333e0fa75395752571c559e4124be0afa4
zub3rrr <xubersworkgmail.com>
2023-06-07 20:42:54 +0530

    added a code to convert requirements.txt into list of values

--- setup.py
+++ setup.py
@@ -1,21 +1,31 @@
 from setuptools import find_packages,setup
 from typing import List
 
-def get_requirements()->List[str]:
-  """Returns a list of requirements.txt"""
-  requirement_list:List[str] = []
+# def get_requirements()->List[str]:
+#   """Returns a list of requirements.txt"""
+#   requirement_list:List[str] = []
   
-  """
-  Write a code so that it returns a list of strings from requirements.txt
-  """
+#   """
+#   Write a code so that it returns a list of strings from requirements.txt
+#   """
    
-  return requirement_list
+#   return requirement_list
+
+requirements_path = r'./requirements.txt'
+def read_requirements_file(file_path):
+    with open(file_path, 'r') as file:
+        requirements = file.read().splitlines()
+    return requirements
+
+
 
 setup(
   name="sensor",
   author="Zuber Ahmed",
   version="0.0.1",
   author_email="xuberswork@gmail.com",
-  packages=find_packages(),   
-  # install_requires=get_requirements(),  or else employ using -e . in requirements.txt
+  # packages=find_packages(),   
+  install_requires=read_requirements_file(requirements_path),  
 )
+
+# #or else employ using -e . in requirements.txt
