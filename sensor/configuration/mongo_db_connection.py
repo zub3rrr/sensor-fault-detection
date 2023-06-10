f8857bdd854cb64e98fd5af3e53e11ca7c66f582
zub3rrr <xubersworkgmail.com>
2023-06-10 20:57:30 +0530

    mondo db connection class created

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -5,21 +5,23 @@ import certifi
 import os
 ca = certifi.where()
 
+
 class MongoDBClient:
     client = None
+
     def __init__(self, database_name=DATABASE_NAME) -> None:
         try:
 
             if MongoDBClient.client is None:
-                mongo_db_url = os.getenv(MONGODB_URL_KEY)
+                mongo_db_url = MONGODB_URL_KEY  # os.getenv(MONGODB_URL_KEY)
                 print(mongo_db_url)
                 if "localhost" in mongo_db_url:
-                    MongoDBClient.client = pymongo.MongoClient(mongo_db_url) 
+                    MongoDBClient.client = pymongo.MongoClient(mongo_db_url)
                 else:
-                    MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
+                    MongoDBClient.client = pymongo.MongoClient(
+                        mongo_db_url, tlsCAFile=ca)
             self.client = MongoDBClient.client
             self.database = self.client[database_name]
             self.database_name = database_name
         except Exception as e:
             raise e
-
